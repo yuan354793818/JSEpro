f167ec66e5e7b23b5b4203df731e29b8b8c53443
yuan354793818 <354793818@qq.com>
2019-02-17 13:28:21 +0800

    ok

--- branchtest/src/main/java/com/yjy/BranchTest.java
+++ branchtest/src/main/java/com/yjy/BranchTest.java
@@ -3,5 +3,7 @@ package com.yjy;
 public class BranchTest {
     public static void main(String[] args) {
         System.out.println("branch1 modified!");
+        System.out.println("branch1 modified!");
+        System.out.println("branch1 modified!");
     }
 }
