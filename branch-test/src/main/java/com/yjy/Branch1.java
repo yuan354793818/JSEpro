c9a74320cc6530ae7de55bf3c7b70899c7d11a7a
yuan354793818 <354793818@qq.com>
2019-02-17 12:24:32 +0800

    ok

---
+++ branch-test/src/main/java/com/yjy/Branch1.java
@@ -0,0 +1,7 @@
+package com.yjy;
+
+public class Branch1 {
+    public static void main(String[] args) {
+        System.out.println("Sdasdas");
+    }
+}
--- maven_test/src/main/java/算法.java
+++ maven_test/src/main/java/算法.java
@@ -312,9 +312,7 @@ public class 算法 {
 
         ListNode n2=new ListNode(head.next.val);
         ListNode n1=new ListNode(head.val);
-        String buf = "sad";
-        String xsadasd = "sds";
-        Integer sds=222;
+
         n2.next=n1;
         n1=n2;
         n2=n2.next;
