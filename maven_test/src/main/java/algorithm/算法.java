f0eb32d2d6eebf53835e85c99e851643a90e32ed
Admin <admin>
2019-08-18 21:37:26 +0800

    342342

--- maven_test/src/main/java/algorithm/算法.java
+++ maven_test/src/main/java/algorithm/算法.java
@@ -3865,6 +3865,74 @@ public class 算法 {
     }
 
 
+    //在一个火车旅行很受欢迎的国度，你提前一年计划了一些火车旅行。在接下来的一年里，你要旅行的日子将以一个名为 days 的数组给出。每一项是一个从 1 到 365 的整数。
+    //
+    //火车票有三种不同的销售方式：
+    //
+    //一张为期一天的通行证售价为 costs[0] 美元；
+    //一张为期七天的通行证售价为 costs[1] 美元；
+    //一张为期三十天的通行证售价为 costs[2] 美元。
+    //通行证允许数天无限制的旅行。 例如，如果我们在第 2 天获得一张为期 7 天的通行证，那么我们可以连着旅行 7 天：第 2 天、第 3 天、第 4 天、第 5 天、第 6 天、第 7 天和第 8 天。
+    //
+    //返回你想要完成在给定的列表 days 中列出的每一天的旅行所需要的最低消费。
+    //
+    // 
+    //
+    //示例 1：
+    //
+    //输入：days = [1,4,6,7,8,20], costs = [2,7,15]
+    //输出：11
+    //解释：
+    //例如，这里有一种购买通行证的方法，可以让你完成你的旅行计划：
+    //在第 1 天，你花了 costs[0] = $2 买了一张为期 1 天的通行证，它将在第 1 天生效。
+    //在第 3 天，你花了 costs[1] = $7 买了一张为期 7 天的通行证，它将在第 3, 4, ..., 9 天生效。
+    //在第 20 天，你花了 costs[0] = $2 买了一张为期 1 天的通行证，它将在第 20 天生效。
+    //你总共花了 $11，并完成了你计划的每一天旅行。
+    //
+    // 动态规划
+    public int mincostTickets(int[] days, int[] costs) {
+        // 将从新年到某一天的花过的所有钱数全部记录起来。
+        int[] lastAllDaysCost = new int[366];
+        //  days的下标，确保遍历365天时，以便于知道下次旅游的日期。
+        int dayIdx = 0;
+        // 日，月，年的花费。
+        int ticketDay = costs[0];
+        int ticketWeek = costs[1];
+        int ticketMonth = costs[2];
+        // 因为是第一天，所以过去的总花费为0
+        lastAllDaysCost[0] = 0;
+        // lastAllCost[i] 是截至到今年的第 i 天的总花费.
+
+        // 模拟新年的第一天跑到旅行的最后一天。
+        for (int today = 1; today <= 365; today++) {
+            if(dayIdx >= days.length){
+                break;
+            }
+            // 判断今天是否属于旅行日。
+            if (days[dayIdx] != today) {
+                // 如果这一天不旅行那么直接把上一天的过去总花费拿过来直接使用。
+                lastAllDaysCost[today] = lastAllDaysCost[today - 1];
+                continue;
+            }
+            // 开始等待下一个待旅行的日子到来。
+            dayIdx++;
+            // 如果一月前，买了月票，会不会更便宜？
+            // 如果一周前，买了周票，会不会更便宜？
+            // 如果都不会的话，那我暂时先买日票试试呗。
+            lastAllDaysCost[today] = Math.min(
+                    Math.min(
+                            lastAllDaysCost[Math.max(0, today - 1)] + ticketDay
+                            , lastAllDaysCost[Math.max(0, today - 7)] + ticketWeek)
+                    , lastAllDaysCost[Math.max(0, today - 30)] + ticketMonth);
+        }
+        return lastAllDaysCost[days[days.length - 1]];
+    }
+
+    @Test
+    public void test3913() {
+        System.out.println(mincostTickets(new int[]{1,4,6,7,8,365},new int[]{2,7,15}));
+    }
+
     //  [1,3,5,6], 4
     // 二分插入位置
     public static int searchInsert(int[] nums, int target) {
